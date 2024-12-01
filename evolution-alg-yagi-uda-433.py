6d0225440df555eca72cbdf878a721b4519f1349
zostaw <zostaw@users.noreply.github.com>
2024-12-02 00:29:24 +0100

    Define chromosome

--- evolution-alg-yagi-uda-433.py
+++ evolution-alg-yagi-uda-433.py
@@ -4,7 +4,7 @@ def handle_nec(result):
     if (result != 0):
         print(necpp.nec_error_message())
 
-def calc_yagi(freq, dipole_length):
+def calc_yagi(freq, chromosome):
 
     segments = 21
     height = 10.2
@@ -12,7 +12,15 @@ def calc_yagi(freq, dipole_length):
 
     
     nec = necpp.nec_create()
-    handle_nec(necpp.nec_wire(nec, 1, segments, -dipole_length/2, height, 0, dipole_length/2, height, 0, wire_thickness, 1, 1))
+    handle_nec(necpp.nec_wire(nec, 1, segments, -chromosome[0][2]/2, height, 0, chromosome[0][2]/2, height, 0, wire_thickness, 1, 1))
+
+    if (len(chromosome) > 0) and chromosome[1][0]:
+        handle_nec(necpp.nec_wire(nec, 1, segments, -chromosome[1][2]/2, height, -chromosome[1][1], chromosome[1][2]/2, height, -chromosome[1][1], wire_thickness, 1, 1))
+    if (len(chromosome) > 1):
+        for director in chromosome[2:]:
+            if director[0]:
+                handle_nec(necpp.nec_wire(nec, 1, segments, -director[2]/2, height, director[1], director[2]/2, height, director[1], wire_thickness, 1, 1))
+
     handle_nec(necpp.nec_geometry_complete(nec, 0))
     handle_nec(necpp.nec_fr_card(nec, 0, 1, freq, 0))
     handle_nec(necpp.nec_ex_card(nec, 0, 1, 11, 1, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0)) 
@@ -31,6 +39,14 @@ def calc_yagi(freq, dipole_length):
 
 
 
+"""
+Gene: binary number
+Chromosome: [dipole, reflector, director1, director2, ...] where each dipole/reflector/director is set of (existance, distance, length) - for dipole existance and distance are ignored
+
+Rules:
+1. if directors are too close to each other - the chromosome dies
+"""
+
 
 if __name__ == "__main__":
     freq = 433.0 # MHz
@@ -38,7 +54,10 @@ if __name__ == "__main__":
     wave_length = c/freq
     dipole_length = 0.5*wave_length
 
-    (z, g) = calc_yagi(freq, dipole_length)
+
+    first_chromosome = [(True, 0, 1e-1), (False, 1e-2, 1e-1), (True, 1e-2, 1e-1), (False, 1e-2, 1e-1), (False, 1e-2, 1e-1), (False, 1e-2, 1e-1)]
+
+    (z, g) = calc_yagi(freq, first_chromosome)
 
     print("Impedance \t(%6.1f,%+6.1fI) Ohms" % (z.real, z.imag))
     print(f"Gain \t {g} dBm")
